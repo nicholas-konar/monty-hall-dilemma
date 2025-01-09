4227848d2878bf5ab9c8a989f18d105da0e09e53
Nicholas Konar <nkonar@protonmail.com>
2025-01-09 15:19:57 -0500

    monty hall dilemma

---
+++ monty-hall.py
@@ -0,0 +1,36 @@
+import random
+
+
+def rand(): return random.randint(0, 2)
+
+
+# random but not equal to x
+def randx(x):
+    y = rand()
+    return y if x != y else randx(x)
+
+
+# 3 doors, one of which contains the car.
+def createDoors():
+    doors = [False] * 3
+    doors[rand()] = True
+    return doors
+
+
+def simulateGame(changeGuess):
+    # randomly generate the doors and make a guess
+    doors = createDoors()
+    guess1 = rand()
+
+    # never reveal the car or guess1
+    n = randx(guess1)
+    reveal = n if not doors[n] else 3 - n - guess1
+
+    # guess2 can only be the last remaining door
+    guess2 = 3 - reveal - guess1
+    return doors[guess2] if changeGuess else doors[guess1]
+
+
+results = [simulateGame(changeGuess=True) for i in range(1000)]
+
+print(f'won {sum(results)} out of {len(results)}')
